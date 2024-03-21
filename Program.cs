8d10357a5a6e9ab2cba15cf024c7ccb76914e73b
brfgomes <bruno_f_gomes@outlook.com>
2024-03-21 14:17:03 -0300

    melhorias e ajustes + arquivo .config

--- Program.cs
+++ Program.cs
@@ -8,45 +8,97 @@ class Program
 {
     static void Main()
     {
-        XmlDocument doc = new XmlDocument();
-        string filePath = "C:\\Users\\dev1\\Desktop\\NFSeSP\\TesteEnvioDeLoteRPS.txt.xml";
-        
+        var filePath = "config.txt";
         if (!File.Exists(filePath))
-            throw new Exception("Caminho de arquivo XML não encontrado!");
-        
-        using (WebClient client = new WebClient())
         {
-            byte[] xmlBytes = client.DownloadData(filePath);
-            
-            doc.LoadXml(Encoding.UTF8.GetString(xmlBytes));
-            
-            string certPath = "C:\\Users\\dev1\\Desktop\\NFSeSP\\cert.pfx";
-            string certPassword = "1234";
-            X509Certificate2 cert = new X509Certificate2(File.ReadAllBytes(certPath), certPassword);
-            
-            SignXmlDocumentWithCertificate(doc, cert);
-
-            Console.WriteLine(doc.OuterXml);
-            File.WriteAllText($"C:\\Users\\dev1\\Desktop\\NFSeSP\\teste\\xmlAssinadoNFSe{new Random().Next(0, 100)}.xml", doc.OuterXml);
+            CreateConfigFile(filePath);
+            Console.WriteLine("Arquivo config não configurado");
+            return;
         }
         
+        var configs = ReadConfigFile(filePath);
+        if (!validateConfigs(configs))
+            return;
+        var doc = new XmlDocument();
+        using var client = new WebClient();
+        {
+            var xmlBytes = client.DownloadData(configs.xmlPathToSign);
+            doc.LoadXml(Encoding.UTF8.GetString(xmlBytes));
+            var cert = new X509Certificate2(File.ReadAllBytes(configs.certPath), configs.certPassword);
+            SignXmlDocumentWithCertificate(doc, cert);
+            File.WriteAllText(configs.signedXmlPath, doc.OuterXml);
+            Console.WriteLine("XML Assinado com sucesso!");
+        }
         static void SignXmlDocumentWithCertificate(XmlDocument doc, X509Certificate2 cert)
         {
-            SignedXml signedXml = new SignedXml(doc);
+            var signedXml = new SignedXml(doc);
             signedXml.SigningKey = cert.PrivateKey;
-            Reference reference = new Reference();
+            var reference = new Reference();
             reference.Uri = "";
             reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
             signedXml.AddReference(reference);
-
-            KeyInfo keyinfo = new KeyInfo();
+            var keyinfo = new KeyInfo();
             keyinfo.AddClause(new KeyInfoX509Data(cert));
-
             signedXml.KeyInfo = keyinfo;
             signedXml.ComputeSignature();
-            XmlElement xmlSig = signedXml.GetXml();
-
-            doc.DocumentElement.AppendChild(doc.ImportNode(xmlSig, true));
+            doc.DocumentElement?.AppendChild(doc.ImportNode( signedXml.GetXml(), true));
+        }
+        static void CreateConfigFile(string filePath)
+        {
+            try
+            {
+                var initialData = "xmlPathToSign;\nsignedXmlPath;\ncertPath;\ncertPassword;";
+                File.WriteAllText(filePath, initialData);
+                Console.WriteLine("Arquivo config.txt criado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao criar o arquivo config.txt: " + ex.Message);
+            }
+        }
+        static configFile ReadConfigFile(string filePath)
+        {
+            var content = File.ReadAllText(filePath);
+            var lines = content.Split('\n');
+            foreach (var line in lines)
+            {
+                var parts = line.Trim().Split(';');
+                if (parts.Length == 4)
+                    return new configFile(parts[0].Trim().Replace(";", ""), parts[1].Trim().Replace(";", ""), parts[2].Trim().Replace(";", ""), parts[3].Trim().Replace(";", ""));
+            }
+            return new configFile(GetValueIfExists(lines, 0).Replace(";", ""), GetValueIfExists(lines, 1).Replace(";", ""), GetValueIfExists(lines, 2).Replace(";", ""), GetValueIfExists(lines, 3).Replace(";", ""));
+        }
+        static string GetPropertyValue(Func<configFile, string> propertySelector, configFile configFile) { return propertySelector(configFile); }
+        static bool validateConfigs(configFile configFile)
+        {
+            List<(Func<configFile, bool> condition, string errorMessage)> validations = new List<(Func<configFile, bool>, string)>
+            {
+                ((cfg) => !string.IsNullOrEmpty(cfg.xmlPathToSign), "Caminho do XML para assinar não informado"),
+                ((cfg) => File.Exists(cfg.xmlPathToSign), "Arquivo do XML para assinar não existe no caminho informado, verifique"),
+                ((cfg) => !string.IsNullOrEmpty(cfg.signedXmlPath), "Caminho para salvar XML assinado não informado"),
+                ((cfg) => File.Exists(cfg.signedXmlPath), "Arquivo para salvar XML assinado não existe no caminho informado, verifique"),
+                ((cfg) => !string.IsNullOrEmpty(cfg.certPath), "Caminho do certificado não informado"),
+                ((cfg) => File.Exists(cfg.certPath), "Certificado para assinar não existe no caminho informado, verifique"),
+                ((cfg) => !string.IsNullOrEmpty(cfg.certPassword), "Senha do certificado não informada")
+            };
+            foreach (var (condition, errorMessage) in validations)
+            {
+                var teste = File.Exists(configFile.xmlPathToSign);
+                var validation = condition(configFile);
+                if (!validation)
+                {
+                    Console.WriteLine(errorMessage);
+                    return false;
+                }
+            }
+            return true;
+        }
+        static string GetValueIfExists(string[] lines, int index)
+        {
+            if (index >= 0 && index < lines.Length)
+                return lines[index];
+            return null;
         }
     }
+    private record configFile(string xmlPathToSign, string signedXmlPath, string certPath, string certPassword);
 }
